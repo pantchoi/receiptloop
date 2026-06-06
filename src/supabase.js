b8483e51bbe38afeb4c64ad020b85e0f19705120
Rick <pantchoi123@gmail.com>
2026-06-06 03:14:03 -0300

    arrumando supabasea

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://rykbnumkmnrkhpwdmiui.supabase.co'
-const supabaseKey = 'sb_publishable_RWNNqvcoiT8YEkJW2XzjJA_Gg97MrKp'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)  
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
